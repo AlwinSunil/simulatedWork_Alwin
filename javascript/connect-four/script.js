2dd1a11ceda0e69064310ad76bb423dd6d77648a
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2025-01-23 14:39:48 +0530

    introduction_to_connect_four

---
+++ javascript/connect-four/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const squares = document.querySelectorAll(".grid div");
+	const result = document.querySelector("#result");
+	const displayCurrentPlayer = document.querySelector("#current-player");
+
+	let currentPlayer = 1;
+
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].onClick = () => {
+			if (squares[i + 7].classList.contains("taken")) {
+				return;
+			}
+		};
+	}
+});
